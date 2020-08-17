fbb36c3ebf4628ce3df25e361fd73cd75f3eda8e
Chris <c.lawsonnelson@gmail.com>
2020-08-17 19:40:38 -0400

    refactor the jsx block rendered by our class component to the return statement of our function component

--- src/components/App.js
+++ src/components/App.js
@@ -19,39 +19,29 @@ const App = () => {
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
-};
-
-class App extends React.Component {
-  onVideoSelect = async (video) => {
-    this.setState({ selectedVideo: video });
-    console.log(this.state.selectedVideo);
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videos}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  const onVideoSelect = async (video) => {
+    setSelectedVideo(video);
+  };
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
